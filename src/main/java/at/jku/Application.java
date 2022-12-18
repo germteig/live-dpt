5572eba60038e7320f5cc256d64b0af535f1370a
germteig <k11770214@students.jku.at>
2022-12-18 23:27:39 +0100

    Initial commit

---
+++ src/main/java/at/jku/Application.java
@@ -0,0 +1,49 @@
+package at.jku;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.fusesource.mqtt.client.BlockingConnection;
+import org.fusesource.mqtt.client.MQTT;
+import org.fusesource.mqtt.client.QoS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
+
+@SpringBootApplication
+public class Application {
+    private static final String power_usage = "power";
+    private static final String order_id = "rfidID";
+    private static final String name = "Stampfmaschine";
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String... args) {
+    SpringApplication.run(Application.class, args);
+  }
+    @Bean
+    public JavaDelegate randomPowerConsumption(){
+        return execution -> {
+            execution.setVariable(power_usage, new Random().nextLong());
+            while (Long.valueOf(execution.getVariable(power_usage).toString()) <0){
+                execution.setVariable(power_usage, new Random().nextLong());
+            }
+        };
+    }
+  @Bean
+  public JavaDelegate helloMQTT(){
+    return execution -> {
+      LOGGER.debug("Starting with MQTT");
+        final MQTT mqtt = new MQTT();
+        mqtt.setHost("localhost", 1883);
+        final BlockingConnection connection = mqtt.blockingConnection();
+        connection.connect();
+        execution.getVariable(order_id);
+        String value = String.valueOf(execution.getVariable(power_usage));
+        connection.publish("Stampfmaschine/Stromverbrauch", value.getBytes(), QoS.AT_LEAST_ONCE, false);
+        LOGGER.debug("Published {} to {}");
+    };
+  }
+}
